a4f6ca49581e22fa0ef369d54f99ae1fd8a4e6be
lhy1205 <972982286@qq.com>
2022-08-10 18:40:36 +0800

    Changes

--- src/pages/HouseList/components/Filter/index.js
+++ src/pages/HouseList/components/Filter/index.js
@@ -5,23 +5,180 @@ import styles from './index.module.css'
 import FilterTitle from "../FilterTitle";
 import FilterPicker from "../FilterPicker";
 import FilterMore from "../FilterMore";
+import {API} from "../../../../utils/api";
+
+const titleSelectedStatus = {
+    area: false,
+    mode: false,
+    price: false,
+    more: false,
+}
+
+const selectedValue = {
+    area: ['area', 'null'],
+    mode: ['null'],
+    price: ['null'],
+    more: [],
+}
 
 export default class Filter extends React.Component {
+    state = {
+        titleSelectedStatus,
+        openType: '',
+        filtersData: {},
+        selectedValue,
+    }
+
+    async getFiltersData() {
+        const {value} = JSON.parse(localStorage.getItem('hkzf_city'));
+        const res = await API.get(`/houses/condition?id=${value}`)
+        console.log(res)
+        this.setState({
+            filtersData: res.data.body
+        })
+    }
+
+    async componentDidMount() {
+        await this.getFiltersData()
+    }
+
+    onTitleClick = (type) => {
+        const {titleSelectedStatus, selectedValue} = this.state
+        let newTitleSelectedStatus = {...titleSelectedStatus}
+        Object.keys(titleSelectedStatus).forEach(key => {
+            if (key === type) {
+                newTitleSelectedStatus[type] = true;
+            } else {
+                if (key === 'area' && (selectedValue[key].length !== 2 || selectedValue[key][0] !== 'area')) {
+                    newTitleSelectedStatus[key] = true
+                } else if (key === 'mode' && selectedValue[key][0] !== 'null') {
+                    newTitleSelectedStatus[key] = true
+                } else if (key === 'price' && selectedValue[key][0] !== 'null') {
+                    newTitleSelectedStatus[key] = true
+                } else if (key === 'more' && selectedValue[key].length > 0) {
+                    newTitleSelectedStatus[key] = true
+                } else {
+                    newTitleSelectedStatus[key] = false
+                }
+            }
+        })
+        this.setState((preState) => {
+            return {
+                // titleSelectedStatus: {
+                //     ...preState.titleSelectedStatus,
+                //     [type]: true
+                // },
+                titleSelectedStatus: newTitleSelectedStatus,
+                openType: type,
+            }
+        })
+    }
+
+    onCancel = (type) => {
+        this.setState({
+            openType: ''
+        })
+    }
+
+    onSave = (type, value) => {
+        console.log(type,value)
+        // 设置选中项为默认时，标题不高亮
+        const {titleSelectedStatus} = this.state
+        let newTitleSelectedStatus = {...titleSelectedStatus}
+        if (type === 'area' && value[0] === 'area' && value.length === 2) {
+            newTitleSelectedStatus[type] = false
+        } else if (type === 'mode' && value[0] === 'null') {
+            newTitleSelectedStatus[type] = false
+        } else if (type === 'price' && value[0] === 'null') {
+            newTitleSelectedStatus[type] = false
+        } else if (type === 'more' && value.length === 0) {
+            newTitleSelectedStatus[type] = false
+        }
+        this.setState({
+            openType: '',
+            selectedValue: {
+                ...this.state.selectedValue,
+                [type]: value
+            },
+            titleSelectedStatus: newTitleSelectedStatus,
+        });
+    }
+
+    renderFilterPicker() {
+        const {openType, filtersData: {area, subway, rentType, price}, selectedValue} = this.state
+        if (openType !== 'area' && openType !== 'mode' && openType !== 'price') {
+            return null
+        }
+        let data = []
+        let cols = 3
+        let defaultValue = selectedValue[openType]
+        switch (openType) {
+            case 'area':
+                data = [area, subway]
+                cols = 3
+                break
+            case 'mode':
+                data = rentType
+                cols = 1
+                break
+            case 'price':
+                data = price
+                cols = 1
+                break
+            case 'more':
+                data = []
+                break
+            default:
+                data = []
+                break
+        }
+        return <FilterPicker
+            onCancel={this.onCancel}
+            onSave={this.onSave}
+            data={data}
+            cols={cols}
+            type={openType}
+            defaultValue={defaultValue}
+        />
+    }
+
+    renderFilterMore() {
+        const {openType, filtersData: {characteristic, floor, oriented, roomType}} = this.state
+        if (openType !== 'more') {
+            return null
+        }
+        let defaultValue = selectedValue[openType]
+        const data = {
+            characteristic,
+            floor,
+            oriented,
+            roomType
+        }
+        return <FilterMore type={openType} data={data} defaultValue={defaultValue} cancelText='清除' onSave={this.onSave}/>
+    }
+
     render() {
+        const {titleSelectedStatus, openType} = this.state
         return (
             <div className={styles.root}>
                 {/* 前三个菜单的遮罩层 */}
-                {/*<div className={styles.mask}></div>*/}
+                {
+                    (openType === 'area' || openType === 'mode' || openType === 'price')
+                        ? <div className={styles.mask} onClick={this.onCancel}></div>
+                        : null
+                }
 
                 <div className={styles.content}>
                     {/* 标题栏 */}
-                    <FilterTitle />
+                    <FilterTitle titleSelectedStatus={titleSelectedStatus} onClick={this.onTitleClick}/>
 
                     {/* 前三个菜单对应的内容： */}
-                    <FilterPicker />
+                    {this.renderFilterPicker()}
 
                     {/* 最后一个菜单对应的内容： */}
-                    {/*<FilterMore />*/}
+                    {
+                        this.renderFilterMore()
+                    }
                 </div>
             </div>
         )
--- src/pages/HouseList/components/FilterMore/index.js
+++ src/pages/HouseList/components/FilterMore/index.js
@@ -4,14 +4,59 @@ import styles from './index.module.css'
 
 export default class FilterMore extends React.Component {
 
+    state = {
+        selectedValues: this.props.defaultValue,
+    }
+
+    onTagClick(value) {
+        const {selectedValues} = this.state
+        let newSelectedValues = [...selectedValues]
+        if (selectedValues.indexOf(value) <= -1) {
+            newSelectedValues.push(value);
+        } else {
+            const index = newSelectedValues.findIndex(item => item === value)
+            newSelectedValues.splice(index, 1)
+        }
+        this.setState({
+            selectedValues: newSelectedValues,
+        })
+    }
+
     // 渲染标签
-    renderFilters() {
-        return  (
-            <span className={[styles.tag, styles.tagActive].join(' ')}>东北</span>
-        )
+    renderFilters(data) {
+        const {selectedValues} = this.state
+        return data.map(item => {
+            const isSelected = selectedValues.indexOf(item.value) > -1 ? true : false
+            return (
+                <span
+                    key={item.value}
+                    className={[
+                        styles.tag,
+                        isSelected ? styles.tagActive : ''
+                    ].join(' ')}
+                    onClick={() => {
+                        this.onTagClick(item.value)
+                    }}
+                >
+                    {item.label}
+                </span>
+            )
+        })
+    }
+
+    onCancel = () => {
+        this.setState({
+            selectedValues: []
+        })
+    }
+
+    onOk = () => {
+        const {onSave, type} = this.props
+        onSave(type, this.state.selectedValues)
     }
 
     render() {
+        const {data: {characteristic, floor, oriented, roomType}, cancelText, onSave, type} = this.props
         return (
             <div className={styles.root}>
                 {/* 遮罩层 */}
@@ -21,21 +66,26 @@ export default class FilterMore extends React.Component {
                 <div className={styles.tags}>
                     <dl className={styles.dl}>
                         <dt className={styles.dt}>户型</dt>
-                        <dd className={styles.dd}>{this.renderFilters()}</dd>
+                        <dd className={styles.dd}>{this.renderFilters(roomType)}</dd>
 
                         <dt className={styles.dt}>朝向</dt>
-                        <dd className={styles.dd}>{this.renderFilters()}</dd>
+                        <dd className={styles.dd}>{this.renderFilters(oriented)}</dd>
 
                         <dt className={styles.dt}>楼层</dt>
-                        <dd className={styles.dd}>{this.renderFilters()}</dd>
+                        <dd className={styles.dd}>{this.renderFilters(floor)}</dd>
 
                         <dt className={styles.dt}>房屋亮点</dt>
-                        <dd className={styles.dd}>{this.renderFilters()}</dd>
+                        <dd className={styles.dd}>{this.renderFilters(characteristic)}</dd>
                     </dl>
                 </div>
 
                 {/* 底部按钮| */}
-                <FilterFooter className={styles.footer} />
+                <FilterFooter
+                    className={styles.footer}
+                    cancelText={cancelText}
+                    onCancel={this.onCancel}
+                    onOk={this.onOk}
+                />
 
             </div>
         )
